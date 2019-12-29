801679b20dd4b122a9547453c012d52091ef43f6
Pratik Narola <narolapratik11@gmail.com>
2019-12-29 14:24:55 +0530

    removing everything in next commit

--- lib/app_screens/first_screen.dart
+++ lib/app_screens/first_screen.dart
@@ -6,16 +6,33 @@ class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Colors.lightBlueAccent,
-      child: Center(
-        child: Text(
-          generateLuckyNumber(),
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-              color: Colors.white, fontSize: 34.0, fontFamily: '8BallScript'),
-        ),
-      ),
-    );
+        color: Colors.lightBlueAccent,
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text(
+                generateLuckyNumber(),
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 34.0,
+                    fontFamily: '8BallScript'),
+              ),
+              Expanded(child:MaterialButton(
+                child: Text(
+                  generateLuckyNumber(),
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 34.0,
+                      fontFamily: '8BallScript'),
+                ),
+                onPressed: () => {},
+                textColor: Colors.white,
+              )
+              )],
+          ),
+        ));
   }
 
   String generateLuckyNumber() {
